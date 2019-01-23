f4e332839710d6ead314d058185ffdfbfd818a93
IVIanuu <IVIanuu@gmail.com>
2019-01-24 00:56:41 +0100

    Update all the things

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -12,9 +12,9 @@ object Build {
 }
 
 object Versions {
-    const val androidGradlePlugin = "3.2.1"
-    const val androidx = "1.0.0"
-    const val kotlin = "1.3.11"
+    const val androidGradlePlugin = "3.3.0"
+    const val androidxAppCompat = "1.0.0"
+    const val kotlin = "1.3.20"
     const val mavenGradle = "2.1"
     const val timber = "4.7.1"
 }
@@ -22,7 +22,7 @@ object Versions {
 object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
 
-    const val androidxAppCompat = "androidx.appcompat:appcompat:${Versions.androidx}"
+    const val androidxAppCompat = "androidx.appcompat:appcompat:${Versions.androidxAppCompat}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
