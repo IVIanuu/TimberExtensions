c70d7c85da66f0fe21095a3c7419c5b1a9d253c7
IVIanuu <IVIanuu@gmail.com>
2019-01-25 15:53:31 +0100

    Gradle changes

--- timberktx/src/main/java/com/ivianuu/timberktx/Timber.kt
+++ timberktx/src/main/java/com/ivianuu/timberktx/Timber.kt
@@ -20,70 +20,64 @@ package com.ivianuu.timberktx
 
 import timber.log.Timber
 
-/**
- * Invokes [Timber.v] if [Timber.treeCount] is > 0
- */
-inline fun v(t: Throwable? = null, message: () -> String) = log { Timber.v(t, message()) }
+/** Invokes [Timber.v] if [Timber.treeCount] is > 0 */
+inline fun v(t: Throwable? = null, message: () -> String) {
+    log { Timber.v(t, message()) }
+}
 
-/**
- * Invokes [Timber.v] if [Timber.treeCount] is > 0
- */
-inline fun v(t: Throwable?) = log { Timber.v(t) }
+/** Invokes [Timber.v] if [Timber.treeCount] is > 0 */
+inline fun v(t: Throwable?) {
+    log { Timber.v(t) }
+}
 
-/**
- * Invokes [Timber.d] if [Timber.treeCount] is > 0
- */
-inline fun d(t: Throwable? = null, message: () -> String) =
+/** Invokes [Timber.d] if [Timber.treeCount] is > 0 */
+inline fun d(t: Throwable? = null, message: () -> String) {
     log { Timber.d(t, message()) }
+}
 
-/**
- * Invokes [Timber.d] if [Timber.treeCount] is > 0
- */
-inline fun d(t: Throwable?) = log { Timber.d(t) }
+/** Invokes [Timber.d] if [Timber.treeCount] is > 0 */
+inline fun d(t: Throwable?) {
+    log { Timber.d(t) }
+}
 
-/**
- * Invokes [Timber.i] if [Timber.treeCount] is > 0
- */
-inline fun i(t: Throwable? = null, message: () -> String) =
+/** Invokes [Timber.i] if [Timber.treeCount] is > 0 */
+inline fun i(t: Throwable? = null, message: () -> String) {
     log { Timber.i(t, message()) }
+}
 
-/**
- * Invokes [Timber.i] if [Timber.treeCount] is > 0
- */
-inline fun i(t: Throwable?) = log { Timber.i(t) }
+/** Invokes [Timber.i] if [Timber.treeCount] is > 0 */
+inline fun i(t: Throwable?) {
+    log { Timber.i(t) }
+}
 
-/**
- * Invokes [Timber.w] if [Timber.treeCount] is > 0
- */
-inline fun w(t: Throwable? = null, message: () -> String) =
+/** Invokes [Timber.w] if [Timber.treeCount] is > 0 */
+inline fun w(t: Throwable? = null, message: () -> String) {
     log { Timber.w(t, message()) }
+}
 
-/**
- * Invokes [Timber.w] if [Timber.treeCount] is > 0
- */
-inline fun w(t: Throwable?) = log { Timber.w(t) }
+/** Invokes [Timber.w] if [Timber.treeCount] is > 0 */
+inline fun w(t: Throwable?) {
+    log { Timber.w(t) }
+}
 
-/**
- * Invokes [Timber.e] if [Timber.treeCount] is > 0
- */
+/** Invokes [Timber.e] if [Timber.treeCount] is > 0 */
 inline fun e(t: Throwable? = null, message: () -> String) =
     log { Timber.e(t, message()) }
 
-/**
- * Invokes [Timber.e] if [Timber.treeCount] is > 0
- */
-inline fun e(t: Throwable?) = log { Timber.e(t) }
+/** Invokes [Timber.e] if [Timber.treeCount] is > 0 */
+inline fun e(t: Throwable?) {
+    log { Timber.e(t) }
+}
 
-/**
- * Invokes [Timber.wtf] if [Timber.treeCount] is > 0
- */
-inline fun wtf(t: Throwable? = null, message: () -> String) =
+/** Invokes [Timber.wtf] if [Timber.treeCount] is > 0 */
+inline fun wtf(t: Throwable? = null, message: () -> String) {
     log { Timber.wtf(t, message()) }
+}
 
-/**
- * Invokes [Timber.wtf] if [Timber.treeCount] is > 0
- */
-inline fun wtf(t: Throwable?) = log { Timber.wtf(t) }
+/** Invokes [Timber.wtf] if [Timber.treeCount] is > 0 */
+inline fun wtf(t: Throwable?) {
+    log { Timber.wtf(t) }
+}
 
 @PublishedApi
 internal inline fun log(block: () -> Unit) {
